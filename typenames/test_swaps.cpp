c5efc734463d0e52b5332662a2e505e30b25d69f
Elton Viana <eltonvs@outlook.com>
2016-03-01 09:58:19 -0300

    Added char to tests

--- typenames/test_swaps.cpp
+++ typenames/test_swaps.cpp
@@ -15,19 +15,23 @@ namespace mySwap {
 }
 
 int main() {
-    int x = 5 , y = 7;
-    double a = 2.3 , b = 4.5;
+    int x = 5, y = 7;
+    double a = 2.3, b = 4.5;
+    char r = 'a', s = 'z';
 
     cout << "Before swap:\n";
     cout << x << " " << y << endl;
     cout << a << " " << b << endl;
+    cout << r << " " << s << endl;
 
     mySwap::swap(x, y);
     mySwap::swap(a, b);
+    mySwap::swap(r, s);
 
     cout << "After swap:\n";
     cout << x << " " << y << endl;
     cout << a << " " << b << endl;
+    cout << r << " " << s << endl;
 
     return EXIT_SUCCESS;
 }
