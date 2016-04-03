db26fadd7e2b7cbbadedf481f0ab00d0e73b1d00
Elton Viana <eltonvs@outlook.com>
2016-04-03 15:41:04 -0300

    Added functors folder to new project

---
+++ functors/messages/include/message.hpp
@@ -0,0 +1,57 @@
+// Copyright 2016
+
+#ifndef _message_hpp_
+#define _message_hpp_
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Message {
+ private:
+    // HEADER
+    std::vector<std::string> msFields;
+
+ public:
+    enum HeaderField {
+        SENDER = 0,   // (FROM) Email of the sender of the message.
+        SUBJECT,      // The message's subject or description of the topic of the message.
+        DATE,         // Date & Time the message was received.
+        RECIPIENT,    // (TO) Email of the recipient of the message.
+        REPLYTO,      // Email that will become the recipiente of any reply to the message.
+        ATTACHMENTS,  // Flag indicating the existance of any files attached to the message.
+        BODY,         // The actual content of the message.
+        N_FIELDS      // Just to keep track of the number of fields.
+    };
+
+ public:
+    Message(std::string = "", std::string = "", std::string = "",
+            std::string = "", std::string = "",
+            std::string = "", std::string = "");
+
+    std::string getHeaderComponent(HeaderField _field) const;
+    std::vector <std::string> getHeader(void) const;
+
+    void setSender(const std::string &_h)    {msFields[HeaderField::SENDER]      = _h;}
+    void setSubject(const std::string &_h)   {msFields[HeaderField::SUBJECT]     = _h;}
+    void setDateTime(const std::string &_h)  {msFields[HeaderField::DATE]        = _h;}
+    void setRecipient(const std::string &_h) {msFields[HeaderField::RECIPIENT]   = _h;}
+    void setReplyTo(const std::string &_h)   {msFields[HeaderField::REPLYTO]     = _h;}
+    void setAttachment(bool _h)              {msFields[HeaderField::ATTACHMENTS] = _h;}
+
+
+    inline friend std::ostream &operator<< (std::ostream &_os, const Message &_Obj) {
+        _os << "{\n"
+            << "  From: ["        << _Obj.msFields[HeaderField::SENDER]      << "]\n"
+            << "  Subject: ["     << _Obj.msFields[HeaderField::SUBJECT]     << "]\n"
+            << "  Date: ["        << _Obj.msFields[HeaderField::DATE]        << "]\n"
+            << "  To: ["          << _Obj.msFields[HeaderField::RECIPIENT]   << "]\n"
+            << "  Reaply: ["      << _Obj.msFields[HeaderField::REPLYTO]     << "]\n"
+            << "  Attachments: [" << _Obj.msFields[HeaderField::ATTACHMENTS] << "]\n"
+            << "  Body:\n "       << _Obj.msFields[HeaderField::BODY]        << "\n}";
+
+        return _os;
+    }
+};
+
+#endif
---
+++ functors/messages/src/driver_message.cpp
@@ -0,0 +1,65 @@
+// Copyright 2016
+
+/**
+ * Exercício
+ * ---------
+ *
+ * 1) Implementar message.cpp de acordo com a especificação de message.h.
+ * 2) Imprimir as mensagens por ordem de
+ *    a) Remetente
+ *    b) Assunto
+ *    c) Data de envio
+ *    utilizando functors.
+ *
+ * 3) Fazer o programa trabalhar com objetos mensagem criado com alocação
+ * dinâmica e ponteiros.
+ *
+ * 4) Converter o programa do passo 2 para trabalhar com std::unique_ptr<>.
+ *
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+#include "message.hpp"
+
+int main() {
+    Message m1( "selan@dimap.ufrn.br", "LP1", "201509011355", "jack@gmail.com", "jack.boy@hotmail.com", "false", "Aqui começa o corpo da mensagem\n Queremos voce por aqui.\n \n Ate breve!" );
+    Message m2( "andre@gmail.com", "Qualquer coisa", "201509101255" );
+    Message m3( "silvia.mota@hotmail.com", "Pesquisa em conjunto", "201509101055" );
+
+    std::vector< Message > ms;
+
+    ms.push_back( m1 );
+    ms.push_back( m2 );
+    ms.push_back( m3 );
+
+    std::cout << ">>> Original messages: \n";
+    for ( auto const & e : ms )
+        std::cout << e << "\n";
+
+    // TODO:
+    // Aqui eh onde você deve ordenar as mensagens armazenadas em ms.
+
+    std::cout << ">>> Sorted messages by Subject: \n";
+    for ( auto const & e : ms )
+        std::cout << e << "\n";
+
+#ifdef _XXXx
+    std::sort(ms.begin(), ms.end(), mySorterDate);
+
+    std::cout << ">>> Sorted messages by Date: \n";
+    for (auto const & e : ms)
+        std::cout << e << "\n";
+
+    std::sort(ms.begin(), ms.end(), mySorterSender);
+
+    std::cout << ">>> Sorted messages by Sender: \n";
+    for (auto const & e : ms)
+        std::cout << e << "\n";
+#endif
+
+    std::cout << ">>> Normal exiting....\n";
+    return EXIT_SUCCESS;
+}
