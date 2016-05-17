4d13b31c1696d9788cd88b18b7631fc815dee5bd
Elton Viana <eltonvs@outlook.com>
2016-05-17 20:51:12 +0000

    Readability improvements on drive code

--- lists/Forward_List/src/drive_forward_list.cpp
+++ lists/Forward_List/src/drive_forward_list.cpp
@@ -17,46 +17,25 @@
 
 int main(int argc, char const *argv[]) {
     Forward_list<int> fl1;
-    // Forward_list<int> fl2(fl1);
-    // Forward_list<int> fl3(std::move(fl1));
 
     // Unity test #1: size()
     assert(fl1.size() == 0);
-    // assert(fl2.size() == 0);
-    // assert(fl3.size() == 0);
 
     // Unity test #2: empty()
     assert(fl1.empty() == true);
-    // assert(fl2.empty() == true);
-    // assert(fl3.empty() == true);
 
     fl1.push_front(3);
-    // fl2.push_front(3);
-    // fl3.push_front(3);
 
-    // Unity test #1: size()
-    assert(fl1.size() == 1);
-    // assert(fl2.size() == 1);
-    // assert(fl3.size() == 1);
-
-    // Unity test #2: empty()
-    assert(fl1.empty() == false);
-    // assert(fl2.empty() == false);
-    // assert(fl3.empty() == false);
-
-    // Unity test #2: empty()
-    assert(fl1.front() == 3);
-    // assert(fl2.front() == 3);
-    // assert(fl3.front() == 3);
+    // Unity test #3: push_front() consequences
+    assert(fl1.size() == 1);       // size()
+    assert(fl1.empty() == false);  // empty()
+    assert(fl1.front() == 3);      // front()
 
     fl1.clear();
-    // fl2.clear();
-    // fl3.clear();
 
-    // Unity test #3: size() and empty() after clear()
-    assert(fl1.size() == 0 && fl1.empty() == true);
-    // assert(fl2.size() == 0 && fl2.empty() == true);
-    // assert(fl3.size() == 0 && fl3.empty() == true);
+    // Unity test #4: clear() consequences
+    assert(fl1.size() == 0);      // size()
+    assert(fl1.empty() == true);  // empty()
 
     std::cout << ">>> Exiting with success...\n";
 
